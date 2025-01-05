de62700cd2f48e2d11487ad2cd249580cd52ac94
Mohammad Shahin <pr.hash2001@gmail.com>
2025-01-05 12:24:01 +0300

    feat(subgraph): add entities: system, CountByMetisAmountStaked

--- subgraph/src/artmetis-staking.ts
+++ subgraph/src/artmetis-staking.ts
@@ -1,11 +1,32 @@
 import { StakedAndLocked as StakedAndLockedEvent, Unlock as UnlockedEvent } from "../generated/StakeAndLock/StakeAndLock";
-import { Participant, StakedAndLocked } from "../generated/schema";
-import { BigInt, Bytes } from "@graphprotocol/graph-ts";
+import { Participant, StakedAndLocked, CountByMetisAmountStaked, System } from "../generated/schema";
+import { BigInt } from "@graphprotocol/graph-ts";
 
-
+let zero = BigInt.fromI32(0);
 let one = BigInt.fromI32(1);
+let oneMetis = BigInt.fromString("1000000000000000000");
+
+function getOrCreateSystem(): System {
+    let sys = System.load("SYSTEM");
+
+    if (!sys) {
+        sys = new System("SYSTEM");
+
+        sys.totalParticipants = 0;
+        sys.totalEligibleParticipants = 0;
+
+        sys.totalMetisStakedByAll = zero;
+        sys.totalArtMetisLockedByAll = zero;
+        sys.totalActionsCountByAll = zero;
+
+        sys.totalUnlockActionsByAll = zero;
+        sys.totalArtMetisUnlockedByAll = zero;
+    }
+    return sys;
+}
 
 export function handleStakedAndLocked(event: StakedAndLockedEvent): void {
+    let system = getOrCreateSystem();
     let entity = new StakedAndLocked(
         event.params.actionId.toHexString()
     );
@@ -27,8 +48,16 @@ export function handleStakedAndLocked(event: StakedAndLockedEvent): void {
     entity.save();
 
 
+    let oldMetisAmounStaked: BigInt;
+    let newMetisAmountStaked: BigInt;
+
     let participant = Participant.load(event.params.user);
     if (participant == null) {
+        system.totalParticipants = system.totalParticipants + 1;
+
+        oldMetisAmounStaked = zero;
+        newMetisAmountStaked = event.params.metisAmount;
+
         participant = new Participant(event.params.user);
         participant.address = event.params.user.toHexString();
         participant.firstBlockNumber = event.block.number;
@@ -37,6 +66,11 @@ export function handleStakedAndLocked(event: StakedAndLockedEvent): void {
         participant.totalActionsCount = one;
     }
     else {
+        oldMetisAmounStaked = participant.totalMetisAmount;
+        newMetisAmountStaked = participant.totalMetisAmount.plus(
+            event.params.metisAmount
+        );
+
         participant.totalMetisAmount = participant.totalMetisAmount.plus(
             event.params.metisAmount
         );
@@ -48,12 +82,54 @@ export function handleStakedAndLocked(event: StakedAndLockedEvent): void {
         );
     }
 
+    system.totalMetisStakedByAll = system.totalMetisStakedByAll.plus(
+        event.params.metisAmount
+    )
+    system.totalArtMetisLockedByAll = system.totalArtMetisLockedByAll.plus(
+        event.params.artMetisAmount
+    )
+    system.totalActionsCountByAll = system.totalActionsCountByAll.plus(
+        one
+    )
     participant.lastBlockNumber = event.block.number;
-
     participant.save();
+
+    // todo: maybe change entities only when amount are more than one because we don't need stakes that are less than one
+    let oldCountByMetisAmountStaked = CountByMetisAmountStaked.load(oldMetisAmounStaked.toHexString());
+    // because amounts can only go up, the only case where this doesn't exist is when the old amount is zero
+    if (oldCountByMetisAmountStaked != null && oldMetisAmounStaked != zero) {
+        oldCountByMetisAmountStaked.count = oldCountByMetisAmountStaked.count - 1;
+    }
+    else {
+        oldCountByMetisAmountStaked = new CountByMetisAmountStaked(oldMetisAmounStaked.toHexString());
+        oldCountByMetisAmountStaked.amount = oldMetisAmounStaked;
+        oldCountByMetisAmountStaked.count = 0;
+    }
+
+    let newCountByMetisAmountStaked = CountByMetisAmountStaked.load(newMetisAmountStaked.toHexString());
+    if (newCountByMetisAmountStaked != null) {
+        newCountByMetisAmountStaked.count = newCountByMetisAmountStaked.count + 1;
+    }
+    else {
+        newCountByMetisAmountStaked = new CountByMetisAmountStaked(newMetisAmountStaked.toHexString());
+        newCountByMetisAmountStaked.amount = newMetisAmountStaked;
+        newCountByMetisAmountStaked.count = 1;
+    }
+
+    // this works because amounts only go up
+    if (oldMetisAmounStaked < oneMetis && newMetisAmountStaked >= oneMetis) {
+        system.totalEligibleParticipants = system.totalEligibleParticipants + 1;
+    }
+
+    oldCountByMetisAmountStaked.save();
+    newCountByMetisAmountStaked.save();
+
+    system.save();
 }
 
 export function handleUnlocked(event: UnlockedEvent): void {
+    let system = getOrCreateSystem();
+
     let entity = new StakedAndLocked(
         event.params.actionId.toHexString()
     );
@@ -64,5 +140,14 @@ export function handleUnlocked(event: UnlockedEvent): void {
     entity.unlockedEventLogIndex = event.logIndex;
     entity.unlocked = true;
 
+    system.totalUnlockActionsByAll = system.totalUnlockActionsByAll.plus(
+        one
+    );
+    system.totalArtMetisUnlockedByAll = system.totalArtMetisUnlockedByAll.plus(
+        event.params.artMetisAmount
+    );
+
+
     entity.save();
+    system.save();
 }
